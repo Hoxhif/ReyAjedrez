59f755e9f293f26c5a873b844ffa3865908fa4be
José A. Guirado González <trizador@gmail.com>
2023-11-22 22:03:21 +0100

    Hemos creado el método de mover, donde basicamente hemos metido lo que teniamos adentro.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -18,11 +18,7 @@ public class MainApp {
             case 2:
                 crearReyColor();
             case 3:
-                try {
-                    rey.mover(Consola.elegirDireccion());
-                }catch (OperationNotSupportedException e){
-                    System.out.println(e.getMessage());
-                }
+                mover();
             case 4:
                 Consola.despedirse();
             default:
@@ -38,6 +34,16 @@ public class MainApp {
     private static void crearReyColor(){
         // Aqui me daba un error, tuve que ir a consola y modificar el método de la opcion de menu y quitar el parametro que tenia.
         rey = new Rey(Consola.elegirOpcion());
+    }
+
+    private static void mover(){
+        try {
+            rey.mover(Consola.elegirDireccion());
+            // Hacemos un sout de rey para que se muestre de nuevo la posicion y demás.
+            System.out.println(rey);
+        }catch (OperationNotSupportedException e){
+            System.out.println(e.getMessage());
+        }
     }
 
 }
