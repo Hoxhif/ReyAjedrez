13773227d33797c10e08649e4ba7988b46e538ae
José A. Guirado González <trizador@gmail.com>
2023-11-22 22:00:06 +0100

    Hemos creado el método de crearReyDefecto, donde basicamente hemos metido lo que teniamos adentro.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -6,29 +6,33 @@ import javax.naming.OperationNotSupportedException;
 
 public class MainApp {
     public static void main(String[] args) {
+        // No podemos poner los métodos dentro del método main porque nos da errores.
+    }
 
-        Rey rey1;
+    private static Rey rey;
 
-        private static void ejecutarOpcion(int opcion){
-            switch(opcion){
-                case 1:
-                     rey1 = new Rey();
-                    System.out.println(rey1);
-                case 2:
-                    // Aqui me daba un error, tuve que ir a consola y modificar el método de la opcion de menu y quitar el parametro que tenia.
-                    rey1 = new Rey(Consola.elegirOpcion());
-                case 3:
-                    try {
-                        rey1.mover(Consola.elegirDireccion());
-                    }catch (OperationNotSupportedException e){
-                        System.out.println(e.getMessage());
-                    }
-                case 4:
-                    Consola.despedirse();
-                default:
-                    Consola.despedirse();
-            }
+    private static void ejecutarOpcion(int opcion){
+        switch(opcion){
+            case 1:
+                crearReyDefecto();
+            case 2:
+                // Aqui me daba un error, tuve que ir a consola y modificar el método de la opcion de menu y quitar el parametro que tenia.
+                rey = new Rey(Consola.elegirOpcion());
+            case 3:
+                try {
+                    rey.mover(Consola.elegirDireccion());
+                }catch (OperationNotSupportedException e){
+                    System.out.println(e.getMessage());
+                }
+            case 4:
+                Consola.despedirse();
+            default:
+                Consola.despedirse();
         }
+    }
 
+    private static void crearReyDefecto(){
+        rey = new Rey();
+        System.out.println(rey);
     }
 }
