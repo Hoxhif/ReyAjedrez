0c18833ae44a2094f8928e5d04fdb3c20b53b759
José A. Guirado González <trizador@gmail.com>
2023-11-22 17:40:31 +0100

    He creado los getters y setters de la clase Posicion. Para ello, le he dado a Code y Generate y he seleccionado los getters y setters para los dos atributos declarados en la Clase. Posteriormente, he modificado los setters y los he puesto como privados como indica el diagrama de clase, y después he hecho las oportunas verificaciones de las excepciones que existen en el tablero de ajedrez a lo que la dirección se refiere de la columna y de la fila.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -6,5 +6,28 @@ public class Posicion {
     private char columna;
 
 
+    public int getFila() {
+        return fila;
+    }
+
+    // Excepcion no controlada IllegalArgumentException, no se pone en el método el throws.
+    private void setFila(int fila) {
+        if (fila<1 || fila>8)
+            throw new IllegalArgumentException("Error: No se puede poner una fila menor que 0 o mayor que 8");
+        this.fila = fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    private void setColumna(char columna) {
+        if(columna>='a' && columna<='h')
+            this.columna = columna;
+        else
+            throw new IllegalArgumentException("Error: las columnas solo van desde la a a la h");
+    }
+
+
 
 }
