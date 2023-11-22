78b24a2d6da07ad5d92a13937a99edb7a14c5d35
José A. Guirado González <trizador@gmail.com>
2023-11-22 17:10:18 +0100

    He añadido la clase Enumeración Direccion y he puesto sus correspondientes literales.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -3,5 +3,8 @@ package org.iesalandalus.programacion.reyajedrez;
 public class MainApp {
     public static void main(String[] args) {
 
+
+
+
     }
 }
---
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Direccion.java
@@ -0,0 +1,16 @@
+package org.iesalandalus.programacion.reyajedrez.modelo;
+
+public enum Direccion {
+    NORTE,
+    SUR,
+    ESTE,
+    OESTE,
+    NORESTE,
+    NOROESTE,
+    SURESTE,
+    SUROESTE,
+    ENROQUE_CORTO,
+    ENROQUE_LARGO;
+
+
+}
