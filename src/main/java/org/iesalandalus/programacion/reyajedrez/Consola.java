41a90bf4ed1c0e411405a6580970674b37c5119d
José A. Guirado González <trizador@gmail.com>
2023-11-22 21:07:30 +0100

    Hemos hecho el método mostrarMenu() en la clase Consola.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
@@ -7,6 +7,14 @@ public class Consola {
 
     }
 
+    public static void mostrarMenu(){
+        System.out.println("---------Selecciona una opción--------");
+        System.out.println("       1- Crear un Rey (Rey por Defecto");
+        System.out.println("       2- Crear un Rey (Elegir el Color");
+        System.out.println("              3- Moverse");
+        System.out.println("                4- Salir");
+    }
+
 
 
 }
